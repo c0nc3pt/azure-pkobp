e7c06dfac21486a62068d09c45a61e88b3dc0dc2
Krystian Zieja <>
2021-01-04 12:28:41 +0100

    variables cleanup

--- development/pkobp/netops/dev/variables.tf
+++ development/pkobp/netops/dev/variables.tf
@@ -290,6 +290,22 @@ variable "nsg_name_10_245_0_0__25" {
     default                 = "nsg_10_245_0_0__25" 
 }
 
+# NOTE: Obiekt definiujacy regule w NSG.
+# nsg_name:                                      Nazwa NSG
+# rg_name:                                       Nazwa resource group
+# name:                                          Nazwa security rule w NSG
+# description:                                   Opis security rule w NSG
+# direction:                                     Kierunek dzialania reguly, mozliwe wartosci Inbound i Outbound
+# priority:                                      Priorytet reguly, moze przyjac wartosci od 100 do 4096. Priorytet musi byc unikalny
+# access:                                        Allow lub Deny dla reguly
+# protocol:                                      Protokol dla reguly, mozliwe wartosci Tcp, Udp, Icmp, lub * (czyli wszystkie protokoly)
+# source_address_prefix:                         Specyfikacja adresow zrodlowych, mozliwe wartosci to adres IP, IP range, * (aby okreslic any), lub jeden z tagow: VirtualNetwork, AzureLoadBalancer and Internet
+# source_port_range:                             Adres lub zakres portow zrodlowych, mozliwe wartosci liczba calkowita lub zakres pomiedzy 0 a 65535 lub *, aby okreslic any
+# destination_address_prefix:                    Specyfikacja adresow docelowych, mozliwe wartosci to adres IP, IP range, * (aby okreslic any), lub jeden z tagow: VirtualNetwork, AzureLoadBalancer and Internet. Dodatkowo mozna wykorzystac Service Tagi: az network list-service-tags -l westeurope
+# destination_port_range:                        Adres lub zakres portow docelowych, mozliwe wartosci liczba calkowita lub zakres pomiedzy 0 a 65535 lub *, aby okreslic any
+# source_application_security_group_ids:         Lista ID Application Security Group okreslajacych zrodlo, jezeli sa uzywane
+# destination_application_security_group_ids:    Lista ID Application Security Group okreslajacych cel, jezeli sa uzywane
+
 locals {
     nsg_rule_object_in_1000__10_245_0_0__25 = {
         nsg_name                                    = module.nsg_10_245_0_0__25.name
@@ -307,18 +323,6 @@ locals {
         source_application_security_group_ids       = []   
         destination_application_security_group_ids  = null
     }
-}
-
-variable "nsg_rule_name_in_1010__10_245_0_0__25" {
-    description = "Nazwa security rule w NSG"
-    type        = string
-    default     = "allow-https-z-10.13.62.100"
-}
-
-variable "nsg_rule_description_in_1010__10_245_0_0__25" {
-    description = "Opis security rule w NSG"
-    type        = string
-    default     = "regula pozwalajaca na https z 10.13.62.100"
 }
 
 locals {
